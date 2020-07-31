5f0e6c7acccd8d78c370fc2175ba07a2f32182b3
Otavio Silva <12450636+otavioguido@users.noreply.github.com>
2020-07-31 16:01:16 -0300

    beginning of 4th class

--- my-app/src/components/FormField/index.js
+++ my-app/src/components/FormField/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function FormField({ label, type, name, value, onChange }){
+    const fieldId = `id_${name}`
     return (
         <div>
-            <label>
+            <label
+                htmlFor={fieldId}
+            >
                 {label}
+                :
                 <input
+                    id={fieldId}
                     type={type}
                     name={name}
                     value={value}
@@ -15,5 +21,19 @@ function FormField({ label, type, name, value, onChange }){
         </div>
     )
 }
+
+FormField.defaultProps = {
+    type: 'text',
+    value: '',
+    onChange: () => {},
+}
+
+FormField.PropTypes = {
+    label: PropTypes.string.isRequired,
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+};
 
 export default FormField;
