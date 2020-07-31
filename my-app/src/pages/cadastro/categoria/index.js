54c7edc0addbdade312f2ec327bbb261ea2a7bab
Otavio Silva <12450636+otavioguido@users.noreply.github.com>
2020-07-31 13:05:35 -0300

    continuing class 3

--- my-app/src/pages/cadastro/categoria/index.js
+++ my-app/src/pages/cadastro/categoria/index.js
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
-    const [categorias, setCategorias] = useState(['Teste']);
     const inicialValues = {
       name: 'Inicial name',
       description: 'Inicial description',
       color: '#bbb',
     }
+
+    const [categorias, setCategorias] = useState([]);
     const [values, setValues] = useState(inicialValues);
+
+
+    function setValue(key, value){
+      setValues({
+        ...values,
+        [key]: value,
+      })
+    }
+
+    function handleChange(data){
+      /* const {getAttribute, value} = data.target; */
+        setValue(
+          data.target.getAttribute('name'), 
+          data.target.value
+      );
+    }
 
     return (
       <PageDefault>
@@ -21,45 +39,30 @@ function CadastroCategoria(){
             ...categorias,
             values
           ]);
-        }}>        
-          <div>
-            <label>
-              Nome da Categoria:
-              <input
-                type="text"
-                value={values.name}
-                onChange={function categoriaInput(data){
-                  setValues(data.target.value)
-                }}
-              />
-            </label>    
-          </div>  
 
-          <div>
-            <label>
-              Descrição:
-              <textarea
-                type="text"
-                value={values.description}
-                onChange={function categoriaInput(data){
-                  setValues(data.target.value)
-                }}
-              />
-            </label>
-          </div>
+          setValues(inicialValues);
+        }}>
 
-          <div>
-            <label>
-              Cor:
-              <input
-                type="color"
-                value={values.color}
-                onChange={function categoriaInput(data){
-                  setValues(data.target.value)
-                }}
-              />
-            </label>
-          </div>
+          <FormField 
+            type="textarea"
+            name="Descrição"
+            value={values.name}
+            onChange={handleChange}
+          />
+
+          <FormField 
+            type="textarea"
+            name="Descrição"
+            value={values.description}
+            onChange={handleChange}
+          />
+
+          <FormField 
+            type="color"
+            name="Cor"
+            value={values.color}
+            onChange={handleChange}
+          />
 
           <button>
             Cadastrar
@@ -70,7 +73,7 @@ function CadastroCategoria(){
           {categorias.map((categoria, indice) => {
             return (
               <li key={`${categoria}${indice}`}>
-                {categoria}
+                {categoria.name}
               </li>
             )
           })}
